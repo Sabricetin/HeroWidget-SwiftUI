ce54fb1f570781a6b27bb98b649f2af61cfbdb69
Sabricetin <sabriyazilim@gmail.com>
2024-08-30 00:44:43 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Sabri Çetin on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
